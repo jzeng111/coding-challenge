b747c8211bc2e9dd8b98ae3884c7d6f9c2e4dc59
jzeng111 <jzeng@supplyframe.com>
2026-01-07 17:08:55 -0800

    use recursion

--- wc/wc.js
+++ wc/wc.js
@@ -1,48 +1,81 @@
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
 
 // first arg, the following, if not check if file path is valid
-const VALID_ARGS = [
-    '-c',
-    '-l',
-    '-w',
-    '-m'
-]
+// const VALID_ARGS = [
+//     '-c',
+//     '-l',
+//     '-w',
+//     '-m'
+// ];
 
-const option = process.argv[2];
-const filePath = path.resolve(process.argv[3]);
-console.log(option, filePath)
-// Sepecific Mode
+// const option = process.argv[2];
+// const filePath = path.resolve(process.argv[3]);
+// console.log(option, filePath);
 
-if (VALID_ARGS.includes(option)) {
-    if (!fs.existsSync(filePath)) {
-        console.error(`Error: can not locate file ${filePath}`)
-    }
+// // With Options
+// if (VALID_ARGS.includes(option)) {
+//     if (!fs.existsSync(filePath)) {
+//         console.error(`Error: can not locate file ${filePath}`);
+//     }
 
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            console.error(`Error: something went wrong while reading file`)
+//     fs.readFile(filePath, (err, data) => {
+//         if (err) {
+//             console.error('Error: something went wrong while reading file');
+//         }
+//         switch (option) {
+//         // get btyes
+//         case '-c':
+//             const bytes = data.length;
+//             console.log(bytes);
+//             break;
+//             // get lines
+//         case '-l':
+//             const lines = data.toString().split('\n').length;
+//             console.log(lines);
+//             break;
+//             // get words
+//         case '-w':
+//             const words = data.toString().split(/\s+/).filter(word => word.length > 0).length;
+//             console.log(words);
+//             break;
+//             // get characters
+//         case '-m':
+//             const chars = data.toString().split('').length;
+//             console.log(chars);
+//             break;
+//         default:
+//             break;
+//         }
+//     });
+//     // Default Mode
+// } else {
+//     console.log('default print');
+// }
+
+
+/**
+ * simulate terminal
+ */
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function askQuestion() {
+    rl.question('What do you think of Node.js? ', (answer) => {
+        if (answer === 'quit()') {
+            rl.close();
+            return;
         }
-        switch (option) {
-            // get btyes
-            case '-c':
-                console.log(data.length)
-                break;
-            case '-l':
-                break;
-            case '-w':
-                break;
-            case '-m':
-                break;
-            default:
-                break
-        }
-    })
-    // Default Mode
-} else {
-    console.log('default print')
+
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+        askQuestion(); // Ask again
+    });
 }
 
+askQuestion();
 
 // const start = performance.now();
 // const end = performance.now();
