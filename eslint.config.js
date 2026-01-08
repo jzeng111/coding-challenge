9bb8a233050292d24bb31f8bcda95e459db0f489
jzeng111 <jzeng@supplyframe.com>
2026-01-07 17:09:42 -0800

    use recursion

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+const js = require('@eslint/js');
+
+module.exports = [
+    js.configs.recommended,
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                Buffer: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+            },
+        },
+        rules: {
+            'indent': ['error', 4],
+            'linebreak-style': ['error', 'unix'],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'always'],
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+        },
+    },
+];
